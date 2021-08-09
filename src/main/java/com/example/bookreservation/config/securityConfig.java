24c048c3c0bfc36a1a496b2ef88744c7faa88a64
KovalevKA <kss47242@gmail.com>
2021-08-09 16:11:45 +0300

    first oauth test
    default settings for recource service

---
+++ src/main/java/com/example/bookreservation/config/securityConfig.java
@@ -0,0 +1,22 @@
+package com.example.bookreservation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class securityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.mvcMatcher("/**")
+                .authorizeRequests()
+                .mvcMatchers("/**")
+                .access("hasRole('ROLE_ADMIN')")
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+    }
+}
