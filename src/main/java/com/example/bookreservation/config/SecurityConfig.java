5795dc51f41e5d40bc76951159443da1aaa83be8
KovalevKA <kss47242@gmail.com>
2021-08-12 15:06:31 +0300

    rename SecurityConfig
    rename class & modify props

--- src/main/java/com/example/bookreservation/config/SecurityConfig.java
+++ src/main/java/com/example/bookreservation/config/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-public class securityConfig {
+public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
