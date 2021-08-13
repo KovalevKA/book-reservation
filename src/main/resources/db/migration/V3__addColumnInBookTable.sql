ffdd9fde1e2f3224cfbf41098397e48947584404
KovalevKA <kss47242@gmail.com>
2021-08-13 16:58:49 +0300

    modify migrations
    add v3 & v4 migration. that be right migration

--- src/main/resources/db/migration/V3__addColumnInBookTable.sql
+++ src/main/resources/db/migration/V3__addColumnInBookTable.sql
@@ -1,1 +1,4 @@
 ALTER TABLE book ADD isbn VARCHAR (50);
+
+UPDATE book
+SET isbn = CONCAT(book_id, '_vc1879esd65c14g5d');
--- src/main/resources/db/migration/V4__addSomeContentIntoNewColumns.sql
+++
@@ -1,3 +0,0 @@
-UPDATE book
-SET isbn = CONCAT (book_id,'sd5s4ds4ds5d4')
-WHERE book_id IN (1, 2, 3);
